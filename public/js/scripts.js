be4b6a6da2cc54f6ceac85818337d947649f8a06
Amit <amitroy@Amits-MacBook-Pro.local>
2018-06-19 13:45:14 +0530

    Slider slidestop event instead of change event

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,13 @@
 $(function () {
-    $("#slider-fill").change((data) => {
+    $("#slider-fill").on("slidestop", function (event) {
         $.post(
             "https://iz7g8bf4t4.execute-api.us-east-1.amazonaws.com/exp/v1/api/led-state",
             JSON.stringify({
                 enableLED: true,
-                brightness: data.target.value
+                brightness: event.target.value
             }),
             function (response) {});
-    })
+    });
     $("#btn-on").click(function (event) {
         $.post("https://iz7g8bf4t4.execute-api.us-east-1.amazonaws.com/exp/v1/api/led-state",
             JSON.stringify({
