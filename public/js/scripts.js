ab027e9cdf8fa3a0451971e42723faf0f5260ca8
Amit <amitroy@Amits-MacBook-Pro.local>
2018-06-19 14:19:04 +0530

    Cache updated to none and smoother slider than before

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,8 +5,7 @@ $(function () {
             JSON.stringify({
                 enableLED: true,
                 brightness: event.target.value
-            }),
-            function (response) {});
+            }));
     });
     $("#btn-on").click(function (event) {
         $.post("https://iz7g8bf4t4.execute-api.us-east-1.amazonaws.com/exp/v1/api/led-state",
